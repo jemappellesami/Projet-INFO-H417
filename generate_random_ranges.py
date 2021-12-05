79915322a4a55eaf1bdf7a47cd13977a4167d5fc
sebadevo <sebastien.de.vos@ulb.be>
2021-12-05 21:05:52 +0100

    frequency histo

--- generate_random_ranges.py
+++ generate_random_ranges.py
@@ -4,13 +4,20 @@ import random as rd
 
 database = pd.DataFrame(columns=["from", "to"])
 i = 0 
+ultimate_max = 10000
+ultimate_min = 0
+
 while i < 1000 :
-    lower = rd.randint(0, 1000)
-    upper = rd.randint(lower, 1001)
-    df2 = {"from":lower, "to":upper}
-
-    database = database.append(df2, ignore_index=True)
-
+    max = rd.randint(2000, ultimate_max)
+    min = rd.randint(ultimate_min, 2000)
+    lower = rd.randint(min, max)
+    upper = rd.randint(min, max)
+    if upper > lower :
+        df2 = {"from":lower, "to":upper}
+        database = database.append(df2, ignore_index=True)
+    elif upper < lower :
+        df2 = {"from":upper, "to":lower}
+        database = database.append(df2, ignore_index=True)
     i += 1
 
-database.to_csv("random_ranges.csv", index=False)
+database.to_csv("random_ranges2.csv", index=False)
--- rangetype.py
+++ rangetype.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 import math
 
-CSV_FILE = "random_ranges_copy.csv"
+CSV_FILE = "random_ranges.csv"
 list_range = []
 """
 Permet d'ouvrir un ficher cvs et de le stocker dans une liste passer en paramètre.
@@ -17,17 +17,25 @@ open_folder_csv(CSV_FILE,list_range)
 list_range.pop(0) # retire le from et to (première ligne du fichier csv)
 
 max = 0
+min = 9999
 for i in list_range: #on cherche la valeur maximum qui determinera la longeur de notre histogram
     if int(i[1])>max:
         max = int(i[1])
-print(max)
+    if int(i[0])<min:
+        min = int(i[0])
+print("min :", min)
+print("max :", max)
+
 nb_bin = 41 # nombre de bins qu'on souhaite avoir
-width = int(1001/nb_bin) # largeur de nos bins
-histo = [0 for i in range(1001+width-1)] # un histograme qui est initialisé comme une liste de 0 de la longueur du équivalent à la valeur maximal dans le ficher csv
+width = int((max-min)/nb_bin) # largeur de nos bins
+print("width :", width)
+# histo = [0 for i in range(1001+width-1)] # un histograme qui est initialisé comme une liste de 0 de la longueur du équivalent à la valeur maximal dans le ficher csv
+histo = [0 for i in range(nb_bin+1)]
 for space in list_range:
     for j in range((int(space[0])-1)//width, math.ceil(int(space[1])/width)):
-        for i in range(width):
-            histo[j*width+i]+=1   #rempli l'histogram
+        histo[j]+=1
+        # for i in range(width):
+        #     histo[j*width+i]+=1   #rempli l'histogram
 
 print(histo)
 # Affichage de l'histograme
@@ -35,12 +43,3 @@ plt.figure()
 plt.plot(histo)
 plt.title('histogram with widths of size : ' + str(width))
 plt.show()
-        
-
-
-
-
-
-
-
-
